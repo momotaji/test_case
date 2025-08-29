571b387c119389ccb816b1a81806e9fb52f7a159
momotaji <momota.clover@icloud.com>
2025-08-29 22:19:18 +0900

    問題２.3 修正

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+#問題1
 def say_hello():
     print('こんにちは！')
 say_hello()
-
+#問題2
 def multiply_numbers(a,b):
-    return a*b
+    result=a*b
+    return result
 print(multiply_numbers(3,5))
+#問題３
+def greet_person(name,age):
+    message=f"{name}と申します。年齢は{age}歳です."
+    return message
+print(greet_person("taji",25))
 
