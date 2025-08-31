b3ec001e76cc99944f823cd68f1abc40e66a83d5
momotaji <momota.clover@icloud.com>
2025-08-31 16:25:41 +0900

    Class Product

--- main.py
+++ main.py
@@ -13,3 +13,18 @@ def greet_person(name,age):
     return message
 print(greet_person("taji",25))
 
+
+class Product:
+    def __init__(self,name,stock):
+        self.name=name
+        self.stock=stock
+
+    def add_stock(self,amount):
+        self.stock +=amount
+
+    def show_stock(self):
+        print(f"{self.name}の在庫は{self.stock}個です")
+
+item=Product("バナナ",5)
+item.add_stock(5)
+item.show_stock()
